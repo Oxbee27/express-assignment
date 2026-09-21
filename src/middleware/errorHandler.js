c13c289f6c775e79a12662318d8c9b3a99ac2970
Ugbah Moses <ugbamoses7@gmail.com>
2026-09-21 12:56:14 +0200

    feat/update

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,6 @@
 const errorHandler = (err, req,res,next) => {
 console.error(err.stack)
 
-
 return res.status(500).json({
 staus: "error",
 message: "internal sever error"
--- src/validators/product.validator.js
+++ src/validators/product.validator.js
@@ -1,48 +1,41 @@
 const validateProduct = (data) => {
-const {name, description, price} = data;
+    const { name, description, price } = data;
 
-if(!name || !description || price){
-throw new Error("name, description and price is required")
+    
+    if (!name || !description || price === undefined) {
+        throw new Error("name, description and price are required");
+    }
 
-}
+   
+    if (typeof name !== "string" || !name.trim()) {
+        throw new Error("product name is required");
+    }
 
-if(typeof name !== "string" || !name.trim()){
+    if (name.trim().length < 2) {
+        throw new Error("product name must be at least 2 characters");
+    }
 
-throw new Error("product name is required")
+    
+    if (typeof description !== "string" || !description.trim()) {
+        throw new Error("product description is required");
+    }
 
-}
+    if (description.trim().length < 6) {
+        throw new Error("product description must be at least 6 characters");
+    }
 
-if(name.length < 2 ){
-throw new Error("product name must be at least more tha 2 characters")
+    
+    if (typeof price !== "number") {
+        throw new Error("price must be a number");
+    }
 
-}
+    if (!Number.isFinite(price)) {
+        throw new Error("price must be a valid number");
+    }
 
-if(typeof description !== "string" || !description.trim()){
-throw new Error("product description is required")
+    if (price < 0) {
+        throw new Error("price must be greater than or equal to zero");
+    }
+};
 
-}
-
-if(description.length < 6){
-throw new Error("product description must be at least 6 characters")
-
-}
-
-if(typeof price !== "Number"){
-throw new Error("price must be a number")
-
-}
-
-if(!Number.isFinite(price)){
-throw new Error("price must be a whole number")
-
-}
-
-if(price < 0){
-throw new Error("price must be greater than zero")
-
-
-}
-
-}
-
-module.exports = {validateProduct}
+module.exports = { validateProduct };
